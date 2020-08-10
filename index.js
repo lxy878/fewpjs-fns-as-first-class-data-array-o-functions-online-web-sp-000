510e2d6d80290afb5fee486da5c766f2c20f2fc4
Xiaoyi Liu <lxy878@gmail.com>
2020-08-10 16:56:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ function wakeDog(dogName, dogBreed){
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function leashDog(){}
-function walkToPark(){}
-function throwFrisbee(){}
-function walkHome(){}
-function unleashDog(){}
+function leashDog(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function walkToPark(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function throwFrisbee(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function walkHome(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function unleashDog(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
