49bcd4407955d34ba3e9e9e08d1ca4f415593cc8
Xiaoyi Liu <lxy878@gmail.com>
2020-08-10 16:55:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function wakeDog(dogName, dogBreed){
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
+
+function leashDog(){}
+function walkToPark(){}
+function throwFrisbee(){}
+function walkHome(){}
+function unleashDog(){}
